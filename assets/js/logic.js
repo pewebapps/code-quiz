58b66f211bb5d471675617ec6d24fef954a7d99b
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-01-22 10:51:56 +0000

    Hide start screen when start button is clicked and show questions screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,10 @@
 // event listener for start button
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", function() {
-    
+    // get start screen div by id
+    // add class hide to remove it being displayed
+    document.getElementById("start-screen").classList.add("hide");
+
+    // get questions section div by id and remove class hide
+    document.getElementById("questions").classList.remove("hide");
 })
