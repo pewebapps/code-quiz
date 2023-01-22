41edb832300f7a456d7a9387aceaf72e011f7bcd
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-01-22 18:28:35 +0000

    Create questions array'

---
+++ assets/js/questions.js
@@ -0,0 +1,203 @@
+const questions = 
+    [
+        {
+            question: "What is JavaScript?",
+            answers: [
+                "JavaScript is a scripting language used to make the website interactive",
+                "JavaScript is an assembly language used to make the website interactive",
+                "JavaScript is a compiled language used to make the website interactive",
+                "None of the mentioned"
+            ],
+            correctAnswerIndex: 0
+        },
+        {
+            question: "Which of the following is correct about JavaScript?",
+            answers: [
+                "JavaScript is an Object-Based language",
+                "JavaScript is Assembly-language",
+                "JavaScript is an Object-Oriented language",
+                "JavaScript is a High-level language"
+            ],
+            correctAnswerIndex: 0
+        },
+        {
+            question: "Arrays in JavaScript are defined by which of the following statements?",
+            answers: [
+                "It is an ordered list of values",
+                "It is an ordered list of objects",
+                "It is an ordered list of string",
+                "It is an ordered list of functions"
+            ],
+            correctAnswerIndex: 0
+        },
+        {
+            question: "Which of the following is not javascript data types?",
+            answers: [
+                "Null type",
+                "Undefined type",
+                "Number type",
+                "All of the mentioned"
+            ],
+            correctAnswerIndex: 3
+        },
+        {
+            question: " Which of the following methods/operation does javascript use instead of == and !=?",
+            answers: [
+                "JavaScript uses equalto()",
+                "JavaScript uses equals() and notequals() instead",
+                "JavaScript uses bitwise checking",
+                "JavaScript uses === and !== instead"
+            ],
+            correctAnswerIndex: 3
+        },
+        {
+            question: "Which of the following variables takes precedence over the others if the names are the same?",
+            answers: [
+                "Global variable",
+                "The local element",
+                "The two of the above",
+                "None of the above"
+            ],
+            correctAnswerIndex: 1
+        },
+        {
+            question: "In JavaScript the x===y statement implies that?",
+            answers: [
+                "Both x and y are equal in value, type and reference address as well.",
+                "Both are x and y are equal in value only",
+                "Both are equal in the value and data type",
+                "Both are not same at all"
+            ],
+            correctAnswerIndex: 2
+        },
+        {
+            question: "Which one of the following is an ternary operator?",
+            answers: [
+                "?",
+                ":",
+                "-",
+                "+"
+            ],
+            correctAnswerIndex: 0
+        },
+        {
+            question: " What we will get if we compare the one with 8 using the less than operator ('one'<8)?",
+            answers: [
+                "False",
+                "True",
+                "NaN",
+                "Undefined"
+            ],
+            correctAnswerIndex: 0
+        },
+        {
+            question: "Which one of the following operator returns false if both values are equal?",
+            answers: [
+                "!",
+                "!==",
+                "!=",
+                "All of the above"
+            ],
+            correctAnswerIndex: 2
+        },
+        {
+            question: "Which one of the following symbol is used for creating comments in the javascript?",
+            answers: [
+                "\\",
+                "//",
+                `\\`
+                `\**/`
+            ],
+            correctAnswerIndex: 1
+        },
+        {
+            question: "What are the three important manipulations for a loop on a loop variable?",
+            answers: [
+                "Updation, Incrementation, Initialization",
+                "Initialization, Testing, Incrementation",
+                "Testing, Updation, Testing",
+                "Initialization, Testing, Updation"
+            ],
+            correctAnswerIndex: 1
+        },
+        {
+            question: "A set of unordered properties that, has a name and value is called______?",
+            answers: [
+                "String",
+                "Array",
+                "Serialized Object",
+                "Object"
+            ],
+            correctAnswerIndex: 3
+        },
+        {
+            question: "Which one of the following method or operator is used for identification of the array?",
+            answers: [
+                "Typeof",
+                "==",
+                "===",
+                "isarrayType()"
+            ],
+            correctAnswerIndex: 3
+        },
+        {
+            question: "What happens if the return statement has no related expression?",
+            answers: [
+                "It will return a undefined value",
+                "It will throw a exception",
+                "It will return the 0 as the value",
+                "It will throw a error"
+            ],
+            correctAnswerIndex: 0
+        },
+        {
+            question: " In JavaScript, do the functions always return a value?",
+            answers: [
+                "Yes, functions always returns a value",
+                "No, it is not necessary",
+                "A number of functions return values by default",
+                "some functions do not return any value"
+            ],
+            correctAnswerIndex: 2
+        },
+        {
+            question: "Which HTML element is used to put the JavaScript code?",
+            answers: [
+                "<javascript>",
+                "<js>",
+                "<scripting>",
+                "<script>"
+            ],
+            correctAnswerIndex: 3
+        },
+        {
+            question: "Which of the following is not a JavaScript Data Types?",
+            answers: [
+                "Boolean",
+                "Undefined",
+                "Number",
+                "Float"
+            ],
+            correctAnswerIndex: 3
+        },
+        {
+            question: "Which of the following function of the Array object is used to add one or more elements to the front of an array and returns the new length of the array?",
+            answers: [
+                "splice()",
+                "unshift()",
+                "sort()",
+                "sort()"
+            ],
+            correctAnswerIndex: 1
+        },
+        {
+            question: "Which of the following syntax can be used to write 'Hello World' in an alert box?",
+            answers: [
+                "alertBox(`Hello World`);",
+                "msgBox(`Hello World`);",
+                "alert(`Hello World`);",
+                "msg(`Hello World`);"
+            ],
+            correctAnswerIndex: 2
+        }
+    ]
