42e8b1295e0f552b7a37945a90a2e1b697f600f2
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-01-22 11:32:22 +0000

    Reset countdown timer and start it when start button is clicked

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,7 @@
+const MAX_TIME = 60;
+
+let secondsRemaining = 0;
+
 // event listener for start button
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", function() {
@@ -7,4 +11,27 @@ startButton.addEventListener("click", function() {
 
     // get questions section div by id and remove class hide
     document.getElementById("questions").classList.remove("hide");
+
+    // reset timer 
+    resetTimer();
+
+    // start count down timer
+    startCountdownTimer()
 })
+
+const resetTimer = () => {
+    secondsRemaining = MAX_TIME;
+    setCountdownTimer()
+}
+
+const setCountdownTimer = () => {
+    document.getElementById("time").textContent = `${secondsRemaining}`;
+}
+
+const startCountdownTimer = () => {
+    const timer = setInterval(function() {
+        secondsRemaining--;
+        setCountdownTimer();
+
+    }, 1000)
+}
