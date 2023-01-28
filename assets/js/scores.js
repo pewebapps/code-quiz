abbd99cb595009508767ed8026ea3dbe4fb1d35e
Piers Ebdon <37227905+piers12@users.noreply.github.com>
2023-01-28 16:39:04 +0000

    clear local storage when clear button is tapped

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,6 +1,19 @@
 // fetch high scores
 const highScores = JSON.parse(localStorage.getItem("high-scores"));
 
+const configureScoreboard = () => {
+    highScores.sort(compareScores);
+
+    // display high scores in ordered list
+    const highScoreList = document.getElementById("highscores");
+    for (let i = 0; i < highScores.length; i++) {
+        const highScore = highScores[i];
+        const listEl = document.createElement("li")
+        listEl.textContent = `${highScore.initials} - ${highScore.score}`;
+        highScoreList.append(listEl);
+    }
+}
+
 const compareScores = (a, b) => {
     const scoreA = a.score;
     const scoreB = b.score;
@@ -15,14 +28,15 @@ const compareScores = (a, b) => {
 }
 
 // sort high scores
-highScores.sort(compareScores);
-
-// display high scores in ordered list
-console.log(highScores);
-const highScoreList = document.getElementById("highscores");
-for (let i = 0; i < highScores.length; i++) {
-    const highScore = highScores[i];
-    const listEl = document.createElement("li")
-    listEl.textContent = `${highScore.initials} - ${highScore.score}`;
-    highScoreList.append(listEl);
+if (highScores !== null) {
+    configureScoreboard();
 }
+
+
+const clearButton = document.getElementById("clear")
+clearButton.addEventListener("click", function() {
+    localStorage.clear();
+
+    // reload the window so that scores are removed
+    window.location.reload();
+})
